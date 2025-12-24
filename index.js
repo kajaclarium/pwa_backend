c2c35af0c4a704d9f22d7b3f9b63225e234793a2
kajaclarium <kajamoideena@clarium.tech>
2025-12-24 17:13:25 +0530

    new commit

--- index.js
+++ index.js
@@ -261,4 +261,4 @@ app.put("/admin/update-user/:id", auth, async (req, res) => {
 /* ---------------------------------------
       SERVER START
 ---------------------------------------- */
-app.listen(5000, () => console.log("API running on port 5000"));
+// app.listen(5000, () => console.log("API running on port 5000"));
